5ba3cee1e02e8446c0fc22d6021bb085afeb4d9c
babreu-ncsa <babreu@illinois.edu>
2022-04-11 10:19:51 -0500

    add fortran hello serial

---
+++ hello/fortran/serial/hello_serial.f90
@@ -0,0 +1,36 @@
+!!!!
+!! File: hello_serial.f90
+!! Description: Fortran Hello World
+!! Author: Bruno R. de Abreu  |  babreu at illinois dot edu
+!! National Center for Supercomputing Applications (NCSA)
+!!  
+!! Creation Date: Monday, 11th April 2022, 10:15:37 am
+!! Last Modified: Monday, 11th April 2022, 10:18:02 am
+!!  
+!! Copyright (c) 2022, Bruno R. de Abreu, National Center for Supercomputing Applications.
+!! All rights reserved.
+!! License: This program and the accompanying materials are made available to any individual
+!!          under the citation condition that follows: On the event that the software is
+!!          used to generate data that is used implicitly or explicitly for research
+!!          purposes, proper acknowledgment must be provided in the citations section of
+!!          publications. This includes both the author's name and the National Center
+!!          for Supercomputing Applications. If you are uncertain about how to do
+!!          so, please check this page: https://github.com/babreu-ncsa/cite-me.
+!!          This software cannot be used for commercial purposes in any way whatsoever.
+!!          Omitting this license when redistributing the code is strongly disencouraged.
+!!          The software is provided without warranty of any kind. In no event shall the
+!!          author or copyright holders be liable for any kind of claim in connection to
+!!          the software and its usage.
+!!!!
+
+program hello_serial
+    implicit none
+    integer my_rank
+
+    ! get my rank -- serial code has just one process
+    my_rank = 0
+
+    ! print message
+    write(*,*) "Hello from PE", my_rank
+
+end program hello_serial
